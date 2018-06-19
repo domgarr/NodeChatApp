691df11db8d5d0e39d558ea5ad4f175fed86b6b6
Domenic Garreffa <garreffd@uwindsor.ca>
2018-06-19 11:29:26 -0400

    feat: port changes dynamically depending on environment, add package information for deploying to heroku

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 const path = require('path');
+
+//This is the path to the directory containg our index
 const publicPath = path.join(__dirname, '../public');
+
+//The app will user 3000 locally.
+const port = process.env.PORT || 3000;
 
 var express = require('express');
 
 var app = express();
-//By deafult express server index.html without having to mention in
+/*
+Use Express middleware to server files from given path
+By deafult express server index.html without having to mention in
+*/
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log("Server is up on 3000");
+app.listen(port, () => {
+    console.log("Server is up on ${port}");
 });
+
 
